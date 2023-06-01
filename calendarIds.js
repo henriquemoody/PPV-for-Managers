b4e5056867fc76018b1348b5944eec24a9402845
Henrique Moody <henriquemoody@gmail.com>
2023-06-01 07:42:40 +0200

    Configure basic calendar
    Signed-off-by: Henrique Moody <henriquemoody@gmail.com>

--- calendarIds.js
+++ calendarIds.js
@@ -1,7 +1,3 @@
 const CALENDAR_IDS = {
-    /* Add calendars here. 
-  The key (string before ':') is what you name the calendar. 
-  The value (string after ':') is the calendar ID. 
-  e.g. ["My calendar name"]: "abcdefg1234@group.calendar.google.com", */
-    ['Primary']: 'primary',
+    ['Personal']: 'henriquemoody@gmail.com',
 };

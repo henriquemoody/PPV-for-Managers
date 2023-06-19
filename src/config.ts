147df0151416440bbd91ad6f6dd251413dde2128
Henrique Moody <henriquemoody@gmail.com>
2023-06-19 06:33:29 +0200

    Allow overwritting configuration
    Signed-off-by: Henrique Moody <henriquemoody@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import internal from 'stream';
+
 const properties = PropertiesService.getScriptProperties();
 
 export enum LogLevel {
@@ -7,6 +9,16 @@ export enum LogLevel {
     ERROR = 3,
 }
 
+function propertyValue(property: string, defaultValue: any): any {
+    const value = properties.getProperty(property);
+
+    if (value === null) {
+        return defaultValue;
+    }
+
+    return JSON.parse(value);
+}
+
 export const CALENDAR_IDS = JSON.parse(properties.getProperty('CALENDAR_IDS')) as Record<string, string>;
 
 export const DAYS_DATABASE_ID = properties.getProperty('DAYS_DATABASE_ID');
@@ -14,16 +26,16 @@ export const SCHEDULES_DATABASE_ID = properties.getProperty('SCHEDULES_DATABASE_
 export const TASKS_DATABASE_ID = properties.getProperty('TASKS_DATABASE_ID');
 export const WEEKS_DATABASE_ID = properties.getProperty('WEEKS_DATABASE_ID');
 
-export const LOGGER_LEVEL = LogLevel.INFO;
-export const LOGGER_SHOWN_AFTER_ERROR = 5;
+export const LOGGER_LEVEL = <LogLevel>propertyValue('LOGGER_LEVEL', LogLevel.INFO);
+export const LOGGER_SHOWN_AFTER_ERROR = <number>propertyValue('LOGGER_SHOWN_AFTER_ERROR', 5);
 
-export const DATE_TIMEZONE = 'CET';
+export const DATE_TIMEZONE = <string>propertyValue('DATE_TIMEZONE', 'CET');
 
-export const DRY_RUN_MODE = false;
+export const DRY_RUN_MODE = <boolean>propertyValue('DRY_RUN_MODE', false);
 
-export const ARCHIVE_CANCELLED_EVENTS = true;
-export const DELETE_CANCELLED_EVENTS = true;
-export const UPDATE_CHANGED_EVENTS = true;
+export const ARCHIVE_CANCELLED_EVENTS = <boolean>propertyValue('ARCHIVE_CANCELLED_EVENTS', true);
+export const DELETE_CANCELLED_EVENTS = <boolean>propertyValue('DELETE_CANCELLED_EVENTS', true);
+export const UPDATE_CHANGED_EVENTS = <boolean>propertyValue('UPDATE_CHANGED_EVENTS', true);
 
-export const RELATIVE_MAX_DAY = 15;
-export const RELATIVE_MIN_DAY = 0;
+export const RELATIVE_MAX_DAY = <number>propertyValue('RELATIVE_MAX_DAY', 15);
+export const RELATIVE_MIN_DAY = <number>propertyValue('RELATIVE_MIN_DAY', 0);

45176e1bff3cb0e61a5785bcc5ac1149df020d40
Henrique Moody <henriquemoody@gmail.com>
2023-11-30 10:45:06 +0100

    Make configuration more reliable
    There were some issues when trying to turn the value of a few values as
    JSON. Now, it's more explicit when the value is a JSON or not.
    
    Signed-off-by: Henrique Moody <henriquemoody@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -7,37 +7,41 @@ export enum LogLevel {
     ERROR = 3,
 }
 
-function propertyValue(property: string, defaultValue: any): any {
+function propertyValue(property: string, defaultValue?: any): any {
     const value = properties.getProperty(property);
 
     if (value === null) {
         return defaultValue;
     }
 
-    return JSON.parse(value);
+    return value;
 }
 
-export const NOTION_TOKEN = properties.getProperty('NOTION_TOKEN');
+function propertyDecode(property: string, defaultValue: any): any {
+    return JSON.parse(propertyValue(property, defaultValue));
+}
 
-export const CALENDAR_IDS = JSON.parse(properties.getProperty('CALENDAR_IDS')) as Record<string, string>;
+export const NOTION_TOKEN: string = propertyValue('NOTION_TOKEN');
 
-export const CALENDAR_DENY_LIST = propertyValue('CALENDAR_DENY_LIST', []) as Array<string>;
+export const CALENDAR_IDS: Record<string, string> = propertyDecode('CALENDAR_IDS', '[]') as Record<string, string>;
 
-export const DAYS_DATABASE_ID = properties.getProperty('DAYS_DATABASE_ID');
-export const SCHEDULES_DATABASE_ID = properties.getProperty('SCHEDULES_DATABASE_ID');
-export const TASKS_DATABASE_ID = properties.getProperty('TASKS_DATABASE_ID');
-export const WEEKS_DATABASE_ID = properties.getProperty('WEEKS_DATABASE_ID');
+export const CALENDAR_DENY_LIST: Array<string> = propertyDecode('CALENDAR_DENY_LIST', '[]') as Array<string>;
 
-export const LOGGER_LEVEL = <LogLevel>propertyValue('LOGGER_LEVEL', LogLevel.INFO);
-export const LOGGER_SHOWN_AFTER_ERROR = <number>propertyValue('LOGGER_SHOWN_AFTER_ERROR', 5);
+export const DAYS_DATABASE_ID: string = <string>propertyValue('DAYS_DATABASE_ID');
+export const SCHEDULES_DATABASE_ID: string = <string>propertyValue('SCHEDULES_DATABASE_ID');
+export const TASKS_DATABASE_ID: string = <string>propertyValue('TASKS_DATABASE_ID');
+export const WEEKS_DATABASE_ID: string = <string>propertyValue('WEEKS_DATABASE_ID');
 
-export const DATE_TIMEZONE = <string>propertyValue('DATE_TIMEZONE', 'CET');
+export const LOGGER_LEVEL: LogLevel = <LogLevel>propertyDecode('LOGGER_LEVEL', LogLevel.INFO);
+export const LOGGER_SHOWN_AFTER_ERROR: number = <number>propertyDecode('LOGGER_SHOWN_AFTER_ERROR', 5);
 
-export const DRY_RUN_MODE = <boolean>propertyValue('DRY_RUN_MODE', false);
+export const DATE_TIMEZONE: string = <string>propertyValue('DATE_TIMEZONE', 'CET');
 
-export const ARCHIVE_CANCELLED_EVENTS = <boolean>propertyValue('ARCHIVE_CANCELLED_EVENTS', true);
-export const DELETE_CANCELLED_EVENTS = <boolean>propertyValue('DELETE_CANCELLED_EVENTS', true);
-export const UPDATE_CHANGED_EVENTS = <boolean>propertyValue('UPDATE_CHANGED_EVENTS', true);
+export const DRY_RUN_MODE: boolean = <boolean>propertyDecode('DRY_RUN_MODE', false);
 
-export const RELATIVE_MAX_DAY = <number>propertyValue('RELATIVE_MAX_DAY', 15);
-export const RELATIVE_MIN_DAY = <number>propertyValue('RELATIVE_MIN_DAY', 0);
+export const ARCHIVE_CANCELLED_EVENTS: boolean = <boolean>propertyValue('ARCHIVE_CANCELLED_EVENTS', true);
+export const DELETE_CANCELLED_EVENTS: boolean = <boolean>propertyValue('DELETE_CANCELLED_EVENTS', true);
+export const UPDATE_CHANGED_EVENTS: boolean = <boolean>propertyValue('UPDATE_CHANGED_EVENTS', true);
+
+export const RELATIVE_MAX_DAY: number = <number>propertyValue('RELATIVE_MAX_DAY', 15);
+export const RELATIVE_MIN_DAY: number = <number>propertyValue('RELATIVE_MIN_DAY', 0);

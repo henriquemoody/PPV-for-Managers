5d9b5144a40fe596182b6c964b48846b90b11c11
Henrique Moody <henriquemoody@gmail.com>
2023-11-30 17:43:00 +0100

    Do not use "Utilities.formatDate"
    Although that's an useful utility, it makes way harder to test code
    locally.
    
    Signed-off-by: Henrique Moody <henriquemoody@gmail.com>

---
+++ src/helpers/DateFormatter.test.ts
@@ -0,0 +1,23 @@
+import DateFormatter from './DateFormatter';
+
+const date = new Date('2023-11-27T12:30:56+01:00');
+
+test('formats a date object into date', () => {
+    expect(DateFormatter.date(date)).toBe('2023-11-27');
+});
+
+test('formats a date object into datetime', () => {
+    expect(DateFormatter.dateTime(date)).toBe('2023-11-27 12:30:56');
+});
+
+test('formats a date object into pretty date', () => {
+    expect(DateFormatter.prettyDate(date)).toBe('November 27, 2023');
+});
+
+test('formats a date object into pretty week', () => {
+    expect(DateFormatter.prettyWeek(date)).toBe('Week 48, 2023');
+});
+
+test('formats a date object into pretty month', () => {
+    expect(DateFormatter.prettyMonth(date)).toBe('November 2023');
+});
--- src/helpers/DateFormatter.ts
+++ src/helpers/DateFormatter.ts
@@ -1,19 +1,52 @@
 import {DATE_TIMEZONE} from '../config';
 
+const locale = 'en-CA';
+
 export default class DateFormatter {
     static date(date: Date): string {
-        return Utilities.formatDate(date, DATE_TIMEZONE, 'yyyy-MM-dd');
+        return date.toLocaleDateString(locale, {
+            timeZone: DATE_TIMEZONE,
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+        });
     }
     static dateTime(date: Date): string {
-        return Utilities.formatDate(date, DATE_TIMEZONE, 'yyyy-MM-dd HH:mm:ss');
+        return date
+            .toLocaleDateString(locale, {
+                timeZone: DATE_TIMEZONE,
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour12: false,
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+            })
+            .replace(',', '');
     }
     static prettyDate(date: Date): string {
-        return Utilities.formatDate(date, DATE_TIMEZONE, 'MMMM d, yyyy');
+        return date.toLocaleDateString(locale, {
+            timeZone: DATE_TIMEZONE,
+            year: 'numeric',
+            month: 'long',
+            day: '2-digit',
+        });
     }
     static prettyWeek(date: Date): string {
-        return 'Week ' + Utilities.formatDate(date, DATE_TIMEZONE, 'w, yyyy');
+        const dateCopy = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+        dateCopy.setUTCDate(dateCopy.getUTCDate() + 4 - (dateCopy.getUTCDay() || 7));
+
+        const yearStart = new Date(Date.UTC(dateCopy.getUTCFullYear(), 0, 1));
+        const weekNumber = Math.ceil(((dateCopy.getTime() - yearStart.getTime()) / 86400000 + 1) / 7);
+
+        return 'Week ' + weekNumber + ', ' + dateCopy.getUTCFullYear();
     }
     static prettyMonth(date: Date): string {
-        return Utilities.formatDate(date, DATE_TIMEZONE, 'MMMMM, yyyy');
+        return date.toLocaleDateString(locale, {
+            timeZone: DATE_TIMEZONE,
+            year: 'numeric',
+            month: 'long',
+        });
     }
 }

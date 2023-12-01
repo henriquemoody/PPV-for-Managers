ac62f68b0d876fe0fb84d6a411ada773b750fced
Henrique Moody <henriquemoody@gmail.com>
2023-12-01 10:42:20 +0100

    Remove unused queries
    Signed-off-by: Henrique Moody <henriquemoody@gmail.com>

--- src/notion/Task/index.ts
+++ src/notion/Task/index.ts
@@ -1,4 +1,2 @@
-export {default as CanceledQuery} from './CanceledTasksQuery';
 export {default as FromCalendarEventQuery} from './TaskFromCalendarEventQuery';
-export {default as LatestQuery} from './LatestTasksQuery';
 export {default as Page} from './TaskPage';
